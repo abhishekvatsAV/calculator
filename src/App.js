f8fb374b65374e6f5e137db857717391f167bbaf
Abhishek Vats <virus@Abhisheks-MacBook-Air.local>
2022-09-19 18:04:11 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [calc, setCalc] = useState("");
+    const [results, setResults] = useState("");
+
+    const ops = ['/', '*', '+', '-', '.'];
+
+    const updateCalc = (value) => {
+        // this below if condition is when current value is operator and we check if it is of type
+        // 1. no operand before this operator then return 
+        // 2. in previous operand a operator is already applied and we cannot append operator in this case also so, return 
+        if(
+            ops.includes(value) && calc === '' || 
+            ops.includes(value) && calc === ops.includes(calc.slice(-1))
+        ) {
+            return ;
+        }
+        // this below statement is for adding operator to first operand
+        setCalc(calc + value);
+
+        // now if this is actually a no i.e, operand then we have to do calculation
+        if(!ops.includes(value)) {
+            setResults(eval(calc + value).toString());
+        }
+    }
+
+    // for = button when we press '=' we want to see the result
+    const calculate = () => {
+        setCalc(eval(calc).toString());
+    }
+
+    // for DEL button to delete the last inserted/clicked value
+    const deleteLast = () => {
+        if(calc === '') {
+            return;
+        }
+
+        const value = calc.slice(0, -1);
+
+        setCalc(value);
+    }
+
+    const createDigits = () => {
+        const digits = [];
+
+        for (let i = 1; i < 10; i++) {
+            digits.push(
+                <button onClick={() => updateCalc(`${i}`)} key={i}>{i}</button>
+            )
+        }
+        return digits;
+    }
+
+    return (
+        <div className="App">
+            <div className="calculator">
+                <div className="display">
+                    {results ? <span>({results})</span> : ''} &nbsp;
+                    {calc || '0'}
+                </div>
+                <div className="operators">
+                    <button onClick={() => updateCalc('/')}>/</button>
+                    <button onClick={() => updateCalc('*')}>*</button>
+                    <button onClick={() => updateCalc('+')}>+</button>
+                    <button onClick={() => updateCalc('-')}>-</button>
+                    <button onClick={deleteLast}>DEL</button>
+                </div>
+                <div className="digits">
+                    {createDigits()}
+                    <button onClick={() => updateCalc('0')}>0</button>
+                    <button onClick={() => updateCalc('.')}>.</button>
+                    <button onClick={calculate}>=</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
